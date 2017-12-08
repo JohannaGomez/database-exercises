91407da22038c42f11782702db6d72d68c30a57e
Johanna Gomez <pao_gomez1@hotmail.com>
2017-12-08 15:16:30 -0600

    first table created with values inserted

--- albums_migration.sql
+++ albums_migration.sql
@@ -1,14 +1,15 @@
 USE codeup_test_db;
 
+DROP TABLE IF EXISTS album;
 DROP TABLE IF EXISTS albums;
 
-create table IF not EXISTS album (
+create table IF not EXISTS albums (
   id int unsigned NOT NULL AUTO_INCREMENT,
-  artist varchar(50) DEFAULT NULL,
-  name varchar(50) DEFAULT NULL,
-  release_date DATE,
-  sales float,
-  genre varchar(50),
+  artist varchar(50) NOT NULL,
+  name varchar(50) not NULL,
+  release_date INT not NULL,
+  sales float not null,
+  genre varchar(50) not NULL,
 
   PRIMARY KEY (id)
 );
