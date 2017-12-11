1de52211c5254db98a56bff3e0ab5d5375384675
Johanna Gomez <pao_gomez1@hotmail.com>
2017-12-11 10:43:12 -0600

    captions included

--- select_exercise.sql
+++ select_exercise.sql
@@ -1,23 +1,42 @@
 USE codeup_test_db;
 
-
 -- The name of all albums by Pink Floyd.
-SELECT * from albums where artist = 'Pink Floyd';
+SELECT 'The name of all albums by Pink Floyd.' AS 'Caption';
+SELECT *
+FROM albums
+WHERE artist = 'Pink Floyd';
+
 
 -- The year Sgt. Pepper's Lonely Hearts Club Band was released
-SELECT release_date from albums where name = 'Sgt. Pepper''s Lonely Hearts Club Band';
+SELECT 'The year Sgt. Pepper''s Lonely Hearts Club Band was released' AS 'Caption';
+SELECT release_date
+FROM albums
+WHERE name = 'Sgt. Pepper''s Lonely Hearts Club Band';
+
 
 -- The genre for Nevermind
-SELECT genre from albums where name = 'nevermind';
+SELECT 'The genre for Nevermind' AS 'Caption';
+SELECT genre
+FROM albums
+WHERE name = 'Nevermind';
 
 -- Which albums were released in the 1990s
-SELECT * from albums where release_date = '1990';
+SELECT 'Which albums were released in the 1990s' AS 'Caption';
+SELECT *
+FROM albums
+WHERE release_date = '1990';
 
 -- Which albums had less than 20 million certified sales
-select * from albums where sales < 20;
+SELECT 'Which albums had less than 20 million certified sales' AS 'Caption';
+SELECT *
+FROM albums
+WHERE sales < 20;
 
 -- All the albums with a genre of "Rock". Why do these query results not include albums
 -- with a genre of "Hard rock" or "Progressive rock"?
-select * from albums where genre like '%rock%';
+SELECT 'All the albums with a genre of "Rock"' AS 'Caption';
+SELECT *
+FROM albums
+WHERE genre LIKE '%rock%';
 
 
---
+++ update_exercises.sql
@@ -0,0 +1,16 @@
+USE codeup_test_db;
+
+# Write SELECT statements to output each of the following with a caption:
+
+# All albums in your table.
+SELECT * from albums;
+
+# All albums released before 1980
+select * from albums where name = 'Michael Jackson' ;
+# All albums by Michael Jackson
+# After each SELECT add an UPDATE statement to:
+#
+# Make all the albums 10 times more popular (sales * 10)
+# Move all the albums before 1980 back to the 1800s.
+# Change 'Michael Jackson' to 'Peter Jackson'
+# Add SELECT statements after each UPDATE so you can see the results of your handiwork.
