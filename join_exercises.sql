fbf5ae0581b11fdc49fc56367558d0e3f0c46b3d
Johanna Gomez <pao_gomez1@hotmail.com>
2017-12-14 11:30:21 -0600

    bonus included

--- join_exercises.sql
+++ join_exercises.sql
@@ -49,8 +49,18 @@ ORDER BY d.dept_name;
 
 # =====Bonus =======
 # Find the names of all current employees, their department name, and their current manager's name.
-SELECT concat(e.first_name, ' ', e.last_name) AS 'Employee Name', d.dept_name AS 'Department Name', CONCAT(e.first_name, ' ' ,  e.last_name) AS 'Department manager'
+SELECT concat(e.first_name, ' ', e.last_name) AS 'Employee Name', d.dept_name AS 'Department Name', CONCAT(m.first_name, ' ' ,  m.last_name) AS 'Department manager'
 FROM employees AS e
+JOIN dept_emp as de
+  ON de.emp_no = e.emp_no
+JOIN departments AS d
+  ON d.dept_no = de.dept_no
+JOIN dept_manager AS dm
+  ON dm.dept_no = d.dept_no
+JOIN employees AS m
+  ON m.emp_no = dm.emp_no
+WHERE de.to_date = '9999-01-01' AND dm.to_date = '9999-01-01' AND e.last_name = 'Lortz' AND e.first_name = 'Huan'
+ORDER BY d.dept_name;
 
 
 # 240,124 Rows
